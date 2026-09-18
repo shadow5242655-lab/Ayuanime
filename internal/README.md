ebcb2352fd09598a8705d7adca1004f5bc36fe24
shadow5242655-lab <shadow5242655@gmail.com>
2026-09-18 21:07:29 +0530

    Rename visible app branding from Seanime to AYUANIME across the app shell and core branding metadata.

--- internal/README.md
+++ internal/README.md
@@ -2,4 +2,4 @@
 <img src="../docs/images/seanime-logo.png" alt="preview" width="70px"/>
 </p>
 
-<h2 align="center"><b>Seanime Server</b></h2>
+<h2 align="center"><b>AYUANIME Server</b></h2>
--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -7,7 +7,7 @@ import (
 
 const (
 	Version              = "3.10.2"
-	VersionName          = "Saisei"
+	VersionName          = "AYUANIME"
 	GcTime               = time.Minute * 30
 	ConfigFileName       = "config.toml"
 	MalClientId          = "51cb4294feb400f3ddc66a30f9b9a00f"
